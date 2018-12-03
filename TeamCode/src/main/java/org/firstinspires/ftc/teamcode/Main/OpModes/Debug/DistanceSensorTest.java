1dda063739e894f3560014b6b962be3943305122
RishiVeerepalli <rishi.veerepalli@gmail.com>
2018-12-02 20:16:23 -0800

    Added distance sensor test file.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main/OpModes/Debug/DistanceSensorTest.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.Main.OpModes.Debug;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.Main.OpModes.ExtendedOpMode;
+
+@TeleOp(name = "Avocado TeleOp Practice")
+public class DistanceSensorTest extends ExtendedOpMode {
+
+    @Override
+    public void init() {
+
+
+
+
+    }
+
+
+    @Override
+    public void loop() {
+
+        telemetry.addData("Distance:", robot.wallAlignFront.getDistance(DistanceUnit.INCH));
+        telemetry.update();
+
+    }
+}
